128f9e5bbf636221690527c23ef02113095ae6bb
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-03-24 15:53:49 +0100

    test: added testing component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestcompComponent } from '../testcomp/testcomp.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TestcompComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ frontend/src/testcomp/testcomp.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-testcomp',
+  standalone: true,
+  imports: [MatSlideToggleModule],
+  templateUrl: './testcomp.component.html',
+  styleUrl: './testcomp.component.scss'
+})
+export class TestcompComponent {
+
+}
