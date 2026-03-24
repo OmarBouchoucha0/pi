fbfcbaf4f084ee7eb41646874e2cde3610aed6e2
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-03-24 19:27:38 +0100

    feature: login page

---
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Card } from 'primeng/card';
+
+@Component({
+  selector: 'app-login',
+  imports: [Card],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {}
