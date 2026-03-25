d886ae31d2c9166e3b3af4111f79f12e522e4fe1
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-03-25 13:50:59 +0100

    feat: add empty signup component with route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
+import { SignupComponent } from './features/auth/signup/signup.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
