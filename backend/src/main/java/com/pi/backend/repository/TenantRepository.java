430849145b165c42f5b3238d996581c60021b5a4
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-03-26 15:11:08 +0100

    Add Department entity and repository, fix TenantRepository package path

---
+++ backend/src/main/java/com/pi/backend/repository/TenantRepository.java
@@ -0,0 +1,8 @@
+package com.pi.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pi.backend.model.Tenant;
+
+public interface TenantRepository extends JpaRepository<Tenant, Long> {
+}
