e487fb681503f884b62519542a9a97ae7ed67572
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-03-26 15:09:55 +0100

    Add enum types: UserRole, UserStatus, Gender, NurseShift, AdminPrivilege

---
+++ backend/src/main/java/com/pi/backend/model/Gender.java
@@ -0,0 +1,7 @@
+package com.pi.backend.model;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ backend/src/main/java/com/pi/backend/model/UserStatus.java
@@ -0,0 +1,7 @@
+package com.pi.backend.model;
+
+public enum UserStatus {
+    ACTIVE,
+    LOCKED,
+    DISABLED
+}
