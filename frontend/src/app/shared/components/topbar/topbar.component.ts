93c61d131706f28b71df6bcff1e6ab37b68eac48
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-03-29 15:21:16 +0100

    feat: add tooltip to notification icon in topbar

--- frontend/src/app/shared/components/topbar/topbar.component.ts
+++ frontend/src/app/shared/components/topbar/topbar.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
+import { Tooltip } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-topbar',
-  imports: [AvatarModule, ButtonModule, ToolbarModule],
+  imports: [AvatarModule, ButtonModule, ToolbarModule, Tooltip],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss',
 })
